9eb0c977a349c880a3a3cc90076204171adb986b
dapanquevabe <dpanquev@everis.com>
2022-06-21 15:33:24 -0500

    vista con filtros

--- src/main/java/com/dh/bd/test/ms/bd/test/repository/view/IAccommodationViewRepository.java
+++ src/main/java/com/dh/bd/test/ms/bd/test/repository/view/IAccommodationViewRepository.java
@@ -3,10 +3,11 @@ package com.dh.bd.test.ms.bd.test.repository.view;
 import com.dh.bd.test.ms.bd.test.model.view.AccommodationView;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IAccommodationViewRepository extends JpaRepository<AccommodationView,Integer> {
-@Query(value = "select * from vw_accommodation_img",nativeQuery = true)
-    public AccommodationView findByTitle(String title);
+    @Query(value = "select * from vw_accommodation_img where title = :title",nativeQuery = true)
+    public AccommodationView findByTitle(@Param("title") String title);
 }
